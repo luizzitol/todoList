69dac4fd3fba5e2202029a313d4d3aa4c4d6a8c4
luizzitol <62354883+luizzitol@users.noreply.github.com>
2020-04-09 14:32:16 +0100

    Updates

---
+++ precache-manifest.ed10e3e9eca3eae827789117b96c2552.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c4b41739946750d55b645c228beb04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888f4e7cef379214ee60",
+    "url": "/static/js/2.cfa558aa.chunk.js"
+  },
+  {
+    "revision": "5b284c0f097608365239",
+    "url": "/static/js/main.967c37a5.chunk.js"
+  },
+  {
+    "revision": "a37d4b3513ae633e0cef",
+    "url": "/static/js/runtime-main.d656882a.js"
+  }
+]);
